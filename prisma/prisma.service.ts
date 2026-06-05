f9ee607d694f85b8b94b42f996a6cb59bb35be6a
Eduardo sequeira <eduardoasm9@gmail.com>
2026-06-05 18:33:35 -0300

    fix: :ambulance: add better prisma sql
    add prisma better sql for prisma 7

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -1,11 +1,18 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { PrismaClient } from '@prisma/client';
+
+const databaseUrl = process.env.DATABASE_URL ?? 'file:./dev.db';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    const adapter = new PrismaBetterSqlite3({ url: databaseUrl });
+    super({ adapter });
+  }
   async onModuleInit() {
     await this.$connect();
   }
