19cb1a1e2098dcaba7aef005315b8fe43b7df30a
Eduardo sequeira <eduardoasm9@gmail.com>
2026-06-06 18:25:27 -0300

    feat: :sparkles: migration and validation length

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,24 +1,28 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, Length } from 'class-validator';
 
 export class CreateRoleDto {
   @ApiProperty({ example: 'Admin' })
   @IsString()
+  @Length(1, 50)
   @IsNotEmpty()
   name: string;
 
   @ApiPropertyOptional({ example: 'Full access role' })
   @IsOptional()
   @IsString()
+  @Length(1, 500)
   description?: string;
 
   @ApiPropertyOptional({ example: 'system' })
   @IsOptional()
   @IsString()
+  @Length(1, 50)
   type?: string;
 
   @ApiPropertyOptional({ example: 'global' })
   @IsOptional()
   @IsString()
+  @Length(1, 50)
   scope?: string;
 }
